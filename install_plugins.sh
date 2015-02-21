f47bd1112880781faa2f1cf856ad62fe6865d0b6
Armando Basile <armando@integrazioneweb.com>
2015-02-21 18:36:50 +0100

    - added prepare and install scripts

---
+++ install_plugins.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Script to install libraries and config files after build of VDR
+# rel. 0.1.0
+
+VDRVER=2.2.0
+VDRBOX_FOLDER="vdrbox"
+VDRSRC="/$VDRBOX_FOLDER/vdr-$VDRVER/PLUGINS/src"
+VDRLIB="/$VDRBOX_FOLDER/vdr-$VDRVER/PLUGINS/lib"
+
+# dvb api plugin to manage oscam and softcam
+echo "copying libvdr-dvbapi.so.$VDRVER"
+cp -f "$VDRSRC/dvbapi/libvdr-dvbapi.so" "$VDRLIB/libvdr-dvbapi.so.$VDRVER"
+
+# standard vdr plugins
+echo "copying libvdr-epgtableid0.so.$VDRVER"
+cp -f "$VDRSRC/epgtableid0/libvdr-epgtableid0.so" "$VDRLIB/libvdr-epgtableid0.so.$VDRVER"
+
+# info about signal
+echo "copying libvdr-femon.so.$VDRVER"
+cp -f "$VDRSRC/femon/libvdr-femon.so" "$VDRLIB/libvdr-femon.so.$VDRVER"
+
+# update epg
+echo "copying libvdr-loadepg.so.$VDRVER"
+cp -f "$VDRSRC/loadepg/libvdr-loadepg.so" "$VDRLIB/libvdr-loadepg.so.$VDRVER"
+
+# video manage
+echo "copying libvdr-softhddevice.so.$VDRVER"
+cp -f "$VDRSRC/softhddevice/libvdr-softhddevice.so" "$VDRLIB/libvdr-softhddevice.so.$VDRVER"
+
+# show epg interface
+echo "copying libvdr-tvguide.so.$VDRVER"
+cp -f "$VDRSRC/tvguide/libvdr-tvguide.so" "$VDRLIB/libvdr-tvguide.so.$VDRVER"
+
+# skin nOpacity
+echo "copying libvdr-skinnopacity.so.$VDRVER"
+cp -f "$VDRSRC/skinnopacity/libvdr-skinnopacity.so" "$VDRLIB/libvdr-skinnopacity.so.$VDRVER"
+
+# skin flatplus
+echo "copying libvdr-skinflatplus.so.$VDRVER"
+cp -f "$VDRSRC/skinflatplus/libvdr-skinflatplus.so" "$VDRLIB/libvdr-skinflatplus.so.$VDRVER"
+
+# copy icons, themes and logos
+echo "copying skin nopacity icons"
+cp -rf "$VDRSRC/skinnopacity/icons" /usr/local/share/vdr/plugins/skinnopacity/
+echo "copying skin nopacity themes"
+cp -f "$VDRSRC/skinnopacity/themes/*" /etc/vdr/themes/
+echo "copying skin nopacity themes config files"
+cp -f "$VDRSRC/skinnopacity/conf/*" /etc/vdr/plugins/skinnopacity/themeconfigs/
+
+echo "copying tvguide icons"
+cp -rf "$VDRSRC/tvguide/icons" /usr/local/share/vdr/plugins/tvguide/
+echo "copying tvguide themes"
+cp -f "$VDRSRC/tvguide/themes/*" /etc/vdr/themes/
+
+# loadepg config files
+echo "copying loadepg config files"
+cp -f "$VDRSRC/loadepg/conf/*" /etc/vdr/plugins/loadepg/
+
+
+
---
+++ prepare_vdr.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Script for VDR environment preparation
+# rel. 0.1.0
+
+# release to checkout
+VDRVER=2.2.0
+VDRBOX_FOLDER="vdrbox"
+
+
+# detect if is superuser
+if [[ $EUID -ne 0 ]]; then
+   echo -e "This script must be run as root\n"
+   exit 1
+fi
+
+# check for parameter presence
+if [ ! -z $1 ]; then
+    VDRVER=$1
+fi
+
+# create vdrbox folder
+echo -e "checking for /$VDRBOX_FOLDER folder presence"
+if [ ! -d "/$VDRBOX_FOLDER" ]; then
+    echo -e "creating /$VDRBOX_FOLDER folder"
+    mkdir "/$VDRBOX_FOLDER"
+fi
+
+# move into folder
+cd "/$VDRBOX_FOLDER"
+
+# get vdr into specific folder
+echo -e "checking for /$VDRBOX_FOLDER/vdr-$VDRVER folder presence" 
+if [ ! -d "/$VDRBOX_FOLDER/vdr-$VDRVER" ]; then
+    echo -e "get snapshot vdr-$VDRVER from git\n"
+    wget "http://projects.vdr-developer.org/git/vdr.git/snapshot/vdr-vdr-$VDRVER.tar.gz"
+    tar xf "vdr-vdr-$VDRVER.tar.gz"
+    rm -f "vdr-vdr-$VDRVER.tar.gz"
+    mv "vdr-vdr-$VDRVER" "vdr-$VDRVER"
+fi
+
+# move into plugins folder
+cd "vdr-$VDRVER/PLUGINS"
+
+# create lib folder for plugins binaries
+if [ ! -d "lib" ]; then
+    echo -e "creating lib folder for plugins binaries"
+    mkdir lib
+fi
+
+# move into plugins source folder
+cd src
+
+# remove unused plugins
+rm -rf dvbhddevice dvbsddevice hello pictures rcu skincurses
+
+# get plugins from git
+if [ ! -d "softhddevice" ]; then
+    echo -e "\nget softhddevice plugin from git"
+    git clone git://projects.vdr-developer.org/vdr-plugin-softhddevice.git softhddevice
+fi
+
+if [ ! -d "skinnopacity" ]; then
+    echo -e "\nget skin-nopacity plugin from git"
+    git clone git://projects.vdr-developer.org/skin-nopacity.git skinnopacity
+    # set to use graphicsmagick instead imagemagick
+    sed -i 's/IMAGELIB = imagemagick/IMAGELIB = graphicsmagick/g' skinnopacity/Makefile
+fi
+
+if [ ! -d "tvguide" ]; then
+    echo -e "\nget tvguide plugin from git"
+    git clone git://projects.vdr-developer.org/vdr-plugin-tvguide.git tvguide
+    # set to use graphicsmagick instead imagemagick
+    #there is some problem at runtime using graphicsmagick, so for now using imagemagick
+    #sed -i 's/IMAGELIB = imagemagick/IMAGELIB = graphicsmagick/g' tvguide/Makefile
+fi
+
+if [ ! -d "dvbapi" ]; then
+    echo -e "\nget dvbapi plugin from git"
+    git clone https://github.com/manio/vdr-plugin-dvbapi.git dvbapi
+fi
+
+if [ ! -d "loadepg" ]; then
+    echo -e "\nget loadepg plugin from lukkinosat website"
+    wget http://lukkinosat.altervista.org/vdr-loadepg-0.2.5.tgz
+    tar xf vdr-loadepg-0.2.5.tgz
+    mv loadepg-0.2.5 loadepg
+fi
+
+if [ ! -d "femon" ]; then
+    echo -e "\nget femon plugin from rahrenbe website"
+    wget http://www.saunalahti.fi/~rahrenbe/vdr/femon/files/vdr-femon-2.2.0.tgz
+    tar xf vdr-femon-2.2.0.tgz
+    mv femon-2.2.0 femon
+fi
+
+echo -e "\n\nReady to build, go to /$VDRBOX_FOLDER/vdr-$VDRVER folder and launch make\n"
+
